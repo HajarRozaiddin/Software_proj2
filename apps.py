34f513f0c882c39afb825c6dbfc45dda43e9d842
Hajar Rozaiddin <hajar.rozaiddin@gmail.com>
2025-12-28 23:43:28 +0800

    Base, report, report submission success.

--- apps.py
+++ apps.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__, static_folder='static', template_folder='Templates')
 @app.route('/')
 def home():
     return render_template('home.html')
-    
+
+@app.route('/reports')
+def reports():
+    return render_template('reports.html')
+
+@app.route('/reportcomplete')
+def report_complete():
+    return render_template('reportcomplete.html')
+
 if __name__ == '__main__':
     app.run(debug=True) 
-
-
-
-    
