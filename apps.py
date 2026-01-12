15a13a6596bf66e45a33ab1cde0d206e9db56119
Hajar Rozaiddin <hajar.rozaiddin@gmail.com>
2026-01-12 22:43:44 +0800

    DB Cloud connection & log in fix

--- apps.py
+++ apps.py
@@ -1,41 +1,48 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
 import time
-<<<<<<< HEAD
-from flask_sqlalchemy import SQLAlchemy
-=======
 import mysql.connector
-
-db = mysql.connector.connect(
-    host="127.0.0.1",
-    user="root",
-    password="1234",
-    database="incident_management_db"
-)
-mycursor = db.cursor(dictionary=True)
->>>>>>> 32a8e6bec9ab490c713d53c0a962d86d2480c478
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__, static_folder='static', template_folder='Templates')
 app.secret_key = 'secret123'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///csems.db'
-db = SQLAlchemy(app)
+load_dotenv()
 
-class User(db.Model):
-    userid = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    phone = db.Column(db.String(15), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    role = db.Column(db.String(50), nullable=False)  # 'student' or 'staff' or 'admin' or 'security staff'
-    accountstatus = db.Column(db.String(50), nullable=False)  # 'active' or 'inactive'
+db = mysql.connector.connect(
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    database=os.getenv("DB_NAME"),
+    ssl_ca=os.getenv("DB_SSL_CA"),  
+)
 
-class Staff(ForeignKey('user.id'), db.Model):
-    staffid = db.Column(db.Integer, primary_key=True)
-    staffuserid = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    staffdepartment = db.Column(db.String(100), nullable=False)
 
-def _repr__(self):
-    return f"<User {self.userid}>"
+# pool = pooling.MySQLConnectionPool(
+#     pool_name="app_pool",
+#     pool_size=5,
+#     **{k: v for k, v in dbconfig.items() if v}  
+# )
+ 
+mycursor = db.cursor(dictionary=True)
+
+# class User(db.Model):
+#     userid = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+#     email = db.Column(db.String(100), unique=True, nullable=False)
+#     phone = db.Column(db.String(15), unique=True, nullable=False)
+#     password = db.Column(db.String(100), nullable=False)
+#     role = db.Column(db.String(50), nullable=False)  # 'student' or 'staff' or 'admin' or 'security staff'
+#     accountstatus = db.Column(db.String(50), nullable=False)  # 'active' or 'inactive'
+
+# class Staff(ForeignKey('user.id'), db.Model):
+#     staffid = db.Column(db.Integer, primary_key=True)
+#     staffuserid = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#     staffdepartment = db.Column(db.String(100), nullable=False)
+
+# def _repr__(self):
+#     return f"<User {self.userid}>"
 
 
 MAX_ATTEMPTS = 3
@@ -104,15 +111,21 @@ def home():
         return redirect(url_for('login'))
     return render_template('home.html')
 
-@app.route('/reports')
+@app.route('/incidents')
 def reports():
-    return render_template('reports.html')
+    return render_template('incidents.html')
+
+@app.route('/reportform')
+def reportform():
+    return render_template('reportform.html')
 
 @app.route('/reportcomplete')
 def reportcomplete(): 
     return render_template('reportcomplete.html')
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
+
     app.run(debug=True)
+        
